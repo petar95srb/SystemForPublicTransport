feb3edb23326cf8cdb7391e1d3964305f5975d8b
Jovan07 <jocaenimen@gmail.com>
2018-02-15 22:03:03 +0100

    Vehical

---
+++ PublicTransportSystem/MongoLayer/ManipulationModels/VehicalModel.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using MongoLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoLayer.ManipulationModels
+{
+   public static class VehicalModel
+    {
+        public static List<Vehical> GetAllVehical()
+        {
+
+            var connectionString = "mongodb://localhost/?safe=true";
+            var server = MongoServer.Create(connectionString);
+            var db = server.GetDatabase("TransportSystem");
+
+
+            var collectionVehical = db.GetCollection<Vehical>("Vehical");
+
+            return (from v in collectionVehical.AsQueryable<Vehical>() select v).ToList();
+        }
+    }
+}
