3ada9a8a67a152f7aef6db5c9a23d2c9cbf1a51a
Jovan07 <jocaenimen@gmail.com>
2018-02-15 22:54:59 +0100

    RemoveStation

--- PublicTransportSystem/MongoLayer/ManipulationModels/RouteModel.cs
+++ PublicTransportSystem/MongoLayer/ManipulationModels/RouteModel.cs
@@ -83,5 +83,23 @@ namespace MongoLayer.ManipulationModels
             Rout.Stations.Insert(index, new MongoDBRef("Station", StationId));
             collectionRoute.Save(Rout);
         }
+
+        public static void RemoveStation(ObjectId routId,ObjectId StationId)
+        {
+            var connectionString = "mongodb://localhost/?safe=true";
+            var server = MongoServer.Create(connectionString);
+            var db = server.GetDatabase("TransportSystem");
+
+
+            var collectionRoute = db.GetCollection<Route>("Route");
+
+            var Rout = (from r in collectionRoute.AsQueryable<Route>() where r.Id == StationId select r).FirstOrDefault();
+            if (Rout == null)
+            {
+                return;
+            }
+            Rout.Stations.Remove( new MongoDBRef("Station", StationId));
+            collectionRoute.Save(Rout);
+        }
     }
 }
