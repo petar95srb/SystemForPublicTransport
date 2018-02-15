053a23f640a905b66c5be3d1d34fe051699fef12
petar95srb <petar21121995@gmail.com>
2018-02-15 18:13:10 +0100

    asf

---
+++ PublicTransportSystem/PublicTransportSystem/Map.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoLayer.ModelViews;
+
+namespace PublicTransportSystem
+{
+
+    public class Map
+    {
+        public Graphics render;
+
+        public Map(Graphics g)
+        {
+            render = g;
+        }
+
+        public void  clearMap()
+        {
+            render.Clear(Color.White);
+        }
+
+        public void drawLines(RoutView rt,Color lineColor)
+        {
+            
+          //  for(int i=0;i<)
+        }
+
+
+
+
+
+    }
+}
