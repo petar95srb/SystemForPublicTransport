23d157905e9fdd09b72285d21f3e31a9b83f1bda
Jovan07 <jocaenimen@gmail.com>
2018-02-16 20:51:25 +0100

    Transport

--- PublicTransportSystem/MongoLayer/ManipulationModels/TransportModel.cs
+++ PublicTransportSystem/MongoLayer/ManipulationModels/TransportModel.cs
@@ -36,5 +36,30 @@ namespace MongoLayer.ManipulationModels
 
             return transp;
         }
+
+        public static TransportView GetTransport(string Type)
+        {
+            var connectionString = "mongodb://localhost/?safe=true";
+            var server = MongoServer.Create(connectionString);
+            var db = server.GetDatabase("TransportSystem");
+
+
+            var collectionRoute = db.GetCollection<Route>("Route");
+            var collectionVehical = db.GetCollection<Vehical>("Vehical");
+            var collectionTransport = db.GetCollection<Transport>("Transport");
+
+            var transp = collectionTransport.AsQueryable<Transport>().Where(t=>t.Type==Type).Select(t => new TransportView()
+            {
+                Id = t.Id,
+                Type = t.Type,
+                Company = t.Company != null ? db.FetchDBRefAs<Company>(t.Company) : null,
+                DynamicFields = t.DynamicFields,
+                Routs = collectionRoute.AsQueryable<Route>().ToList().Where(p => t.Routs.Contains(new MongoDBRef("Routs", p.Id))).ToList(),
+                Vehicals = collectionVehical.AsQueryable<Vehical>().ToList().Where(v => t.Vehicals.Contains(new MongoDBRef("Vehicals", v.Id))).ToList(),
+
+            }).FirstOrDefault();
+
+            return transp;
+        }
     }
 }
