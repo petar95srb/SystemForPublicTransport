ef4f953403cab047dc03816cfd64f78c004b5bdd
Jovan07 <jocaenimen@gmail.com>
2018-02-16 18:32:58 +0100

    AllVehical

--- PublicTransportSystem/MongoLayer/ManipulationModels/VehicalModel.cs
+++ PublicTransportSystem/MongoLayer/ManipulationModels/VehicalModel.cs
@@ -1,4 +1,5 @@
-﻿using MongoDB.Driver;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
 using MongoDB.Driver.Linq;
 using MongoLayer.Models;
 using System;
@@ -22,6 +23,19 @@ namespace MongoLayer.ManipulationModels
             var collectionVehical = db.GetCollection<Vehical>("Vehical");
 
             return (from v in collectionVehical.AsQueryable<Vehical>() select v).ToList();
+        }
+
+        public static List<Vehical> GetAllVehical(ObjectId TransportId)
+        {
+
+            var connectionString = "mongodb://localhost/?safe=true";
+            var server = MongoServer.Create(connectionString);
+            var db = server.GetDatabase("TransportSystem");
+
+            MongoDBRef Transp = new MongoDBRef("Transport", TransportId);
+            var collectionVehical = db.GetCollection<Vehical>("Vehical");
+
+            return (from v in collectionVehical.AsQueryable<Vehical>() where v.Transport==Transp select v).ToList();
         }
     }
 }
