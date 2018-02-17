5aa207846f32a1d0387fa48a0a031ced300409d0
petar95srb <petar21121995@gmail.com>
2018-02-17 19:40:46 +0100

    sdsds

---
+++ PublicTransportSystem/MongoLayer/ModelViews/RideViewTimeTable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoLayer.ModelViews
+{
+    class RideViewTimeTable
+    {
+        public string Line { get; set; }
+        public string TransportType { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+        public string CurrentStation { get; set; }
+    }
+}
